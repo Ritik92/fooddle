90fb011cda9b2bb9a7ed311a515d5981a8fd65ca
Ritik92 <kartik292004@gmail.com>
2024-07-20 20:30:40 +0530

    implemented auth favicon and description

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,12 @@
+import { Button } from "@nextui-org/react";
 import Image from "next/image";
+import SignIn from "./api/auth/signin/page";
 
 export default function Home() {
   return (
-    <div>
-      Fooddle
-    </div>
+ <div>
+  <SignIn/>
+ </div>
     
   );
 }
