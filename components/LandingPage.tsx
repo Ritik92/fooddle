84b1351ec206439a4f3c7eb895317b96369a13b1
Ritik92 <kartik292004@gmail.com>
2024-07-20 21:24:22 +0530

    signout logic

--- components/LandingPage.tsx
+++ components/LandingPage.tsx
@@ -1,6 +1,7 @@
 "use client"
 import SignIn from '@/app/api/auth/signin/page';
-import { useSession } from 'next-auth/react';
+import { Button } from '@nextui-org/react';
+import { signOut, useSession } from 'next-auth/react';
 
 const Login = () => {
   const { data: session, status } = useSession();
@@ -10,7 +11,9 @@ const Login = () => {
       {status === 'loading' ? (
         <p>Loading...</p>
       ) : session ? (
-        <p>Welcome, {session.user?.name}!</p>
+        <p>Welcome! <Button onClick={()=> signOut()}>SignOut</Button>
+</p>
+        
       ) : (
         <p><SignIn/></p>
       )}
