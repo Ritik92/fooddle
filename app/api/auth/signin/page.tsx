947d1fbe70b9392a470a922d3665f90c98a6d89a
Ritik92 <kartik292004@gmail.com>
2024-07-23 05:11:49 +0530

    made signin page responsive

--- app/api/auth/signin/page.tsx
+++ app/api/auth/signin/page.tsx
@@ -14,11 +14,14 @@ const SignIn = () => {
   const toggleForm = () => setIsLogin(!isLogin);
 
   return (
-    <div className="flex flex-col min-h-screen bg-[#EAF3FF]">
+    <div className="flex flex-col min-h-screen bg-white md:bg-[#EAF3FF]">
       <div className="flex-grow flex flex-col">
-        <div className="p-3 md:p-6">
+        <div className="hidden md:block p-3  md:ml-3 mt-1 ">
           <Image src="/logo.png" alt="Foodle logo" width={128} height={55} className="w-24 md:w-32" />
         </div>
+          <div className="block bg-[#004BAD] md:hidden h-[14.75rem] flex items-center justify-center md:ml-3">
+             <Image src="/logo2.png" alt="Foodle logo" width={150} height={63} className=" md:w-32" />
+          </div>
         <div className="flex flex-col md:flex-row flex-grow">
           <div className="hidden md:block md:basis-1/2 relative">
             <div className="absolute top-[30px] left-[10%] lg:left-[20%]">
@@ -29,16 +32,16 @@ const SignIn = () => {
             </div>
           </div>
           <div className="md:basis-1/2 flex items-center justify-center p-4 md:p-0">
-            <div className="bg-white rounded-lg shadow-md w-full max-w-md overflow-hidden">
-              <div className="flex">
+            <div className="bg-blue md:bg-white  absolute top-[11rem]  md:static  lg:mt-10 rounded-xl  w-full md:max-w-md overflow-hidden">
+              <div className="flex ">
                 <button
-                  className={`flex-1 py-4 text-lg font-semibold ${isLogin ? 'text-blue-600' : 'text-gray-400'}`}
+                  className={`flex-1 py-4 text-lg font-semibold  ${isLogin ? 'text-[#004BAD] bg-white rounded-t-xl ' : 'text-white bg-[#004BAD] md:text-[#4D4D4D] md:bg-white font-urbanist'}`}
                   onClick={() => setIsLogin(true)}
                 >
                   Login
                 </button>
                 <button
-                  className={`flex-1 py-4 text-lg font-semibold ${!isLogin ? 'text-blue-600' : 'text-gray-400'}`}
+                  className={`flex-1 py-4 text-lg font-semibold ${!isLogin ? 'text-[#004BAD]  bg-white rounded-t-xl' : 'text-white md:text-[#4D4D4D] bg-[#004BAD] md:bg-white font-urbanist'}`}
                   onClick={() => setIsLogin(false)}
                 >
                   Sign up
@@ -60,7 +63,7 @@ const SignIn = () => {
           </div>
         </div>
       </div>
-      <div className="text-center text-sm text-gray-600 mt-4 mb-4">
+      <div className="hidden md:block text-center text-sm text-gray-600 mt-4 mb-4 ">
         Need help? <a href="#" className="text-blue-600">Contact Us</a> for support.
       </div>
     </div>
@@ -110,11 +113,11 @@ const LoginForm = ({ toggleForm }: { toggleForm: () => void }) => {
     setShowPassword(!showPassword);
   };
   return (
-    <form className="space-y-4" onSubmit={handleSubmit}>
+    <form className="space-y-4 " onSubmit={handleSubmit}>
       <input 
         type="email" 
         placeholder="Email" 
-        className="w-full p-3 border rounded-md" 
+        className="w-full p-3 border rounded-md " 
         value={email}
         onChange={(e) => setEmail(e.target.value)}
         required
@@ -145,7 +148,7 @@ const LoginForm = ({ toggleForm }: { toggleForm: () => void }) => {
         
         <a href="#" onClick={handleForgotPassword} className="text-sm text-blue-600">Forgot Password?</a>
       </div>
-      <button type="submit" className="w-full bg-blue-600 text-white py-3 rounded-md hover:bg-blue-700 transition-colors">Login</button>
+      <button type="submit" className="w-full bg-blue-600 text-white py-3 rounded-md hover:bg-blue-700  transition-colors">Login</button>
       <div className="relative text-center">
         <hr className="my-4" />
         <span className="bg-white px-2 text-sm text-gray-500 absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2">OR</span>
@@ -154,7 +157,7 @@ const LoginForm = ({ toggleForm }: { toggleForm: () => void }) => {
         <Image src="/google logo.png" alt="Google logo" width={20} height={20} className="mr-2" />
         Log in with Google
       </button>
-      <p className="text-center text-sm">
+      <p className="text-center text-sm ">
         Don't have an account yet? <button type="button" onClick={toggleForm} className="text-blue-600 hover:underline">Sign up</button>
       </p>
     </form>
@@ -223,7 +226,7 @@ const SignupForm = ({ toggleForm }: { toggleForm: () => void }) => {
       {error && (
         <div className="text-red-500 text-sm mt-2">{error}</div>
       )}
-      <button type="submit"  className="w-full bg-blue-600 text-white py-3 rounded-md hover:bg-blue-700 transition-colors">Sign Up</button>
+      <button type="submit"  className="w-full bg-blue-600 text-white py-3 rounded-md hover:bg-blue-700 transition-colors font-urbanist">Sign Up</button>
       <div className="relative text-center">
         <hr className="my-4" />
         <span className="bg-white px-2 text-sm text-gray-500 absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2">OR</span>
