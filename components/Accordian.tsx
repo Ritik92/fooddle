6fedcce5159ed206755fa385b326cfdf1bd5bc52
Rajkaran <mindfulness04@gmail.com>
2024-07-26 02:35:40 -0700

    Accordion responsive

--- components/Accordian.tsx
+++ components/Accordian.tsx
@@ -3,61 +3,86 @@
 import { Accordion, AccordionItem } from "@nextui-org/accordion";
 import React from 'react';
 import { Card, Button, Input } from '@nextui-org/react';
-
+import Profile from "./profile";
 import { IoIosArrowBack } from "react-icons/io";
 
+import ProfileButtons from "./profileButtons";
+import ProfileHeader from "./profileHeader";
+
 const Accordian = () => {
-  
+
 
   const defaultContent =
-  "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.";
+    "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.";
 
   return (
-    <div className="flex flex-col items-center min-h-screen bg-[#FCFCFC]  pt-0 font-urbanist">
-      
-      <div
-        className="w-full  h-[120px] rounded-bl-lg rounded-br-lg bg-[#004BAD] flex items-center justify-center mb-8 absolute pb-6"
-      >
-        <div className="flex items-center w-full px-4 pt-16">
-          <span className="text-white text-xl mr-3"><IoIosArrowBack size={24}/></span>
-          
-          <h2 className="text-white text-xl ">Help & Support</h2>
-        </div>
+    <div className="flex">
+      <div className="hidden md:flex md:items-center md:[15vw]">
+        Fooddle
       </div>
-     <div className='pt-44 w-full text-[#4D4D4D] '> 
-     <div className="w-full px-4 py-6 bg-white ">
-        
-        <h2 className="text-[#4D4D4D] font-bold px--6">FAQs</h2>
-
-        {/* Input Fields */}
-        <div className="flex flex-col items-center justify-center w-full space-y-4 bg-white">
-          <div className="w-full relative p-3">
-          <Accordion selectionMode="multiple">
-                <AccordionItem key="1" aria-label="Kitna kama lete ho?" title="Kitna kama lete ho? ">
-                     {"Bas unicorn banne wale hai thode dino mein"}
-                 </AccordionItem>
-                 <AccordionItem key="2" aria-label="What are the delivery hours?" title="What are the delivery hours?">
-                     {"Our delivery hours are from 10:00 AM to 11:00 PM,seven days a week. Please note that delivery hours may vary slightly on public holidays"}
-                 </AccordionItem>
-                 <AccordionItem key="3" aria-label="What should I do if my payment is declined?" title="What should I do if my payment is declined?">
-                     {defaultContent}
-                 </AccordionItem>
-                 <AccordionItem key="4" aria-label="How do I request a refund?" title="How do I request a refund?">
-                     {defaultContent}
-                 </AccordionItem>
-             </Accordion>
+      <div className="flex flex-col">
+        <div className="hidden md:block">
+          <ProfileHeader />
+        </div>
+        <div className="flex flex-row w-full relative top-0 bg-[#FCFCFC] md:top-[13.5rem]">
+          <div className="hidden md:block md:[10vw]">
+            <ProfileButtons />
           </div>
 
-        </div>
+          <div className="flex flex-col w-[100vw] items-center min-h-screen bg-[#FCFCFC] pt-0 font-urbanist md:w-[88vw]">
 
-        <div className="flex flex-col items-center justify-center mt-20">
-          <div className=""><p>For more queries email us at:</p></div>
-          <div><p className="text-[#004BAD]">contact@fooddle.in</p></div>
+            <div
+              className="w-full  h-[120px] rounded-bl-lg rounded-br-lg bg-[#004BAD] flex items-center justify-center mb-8  pb-6 sm:w-[100vw] md:hidden"
+            >
+              <div className="flex items-center w-full px-4 pt-16 ">
+                <span className="text-white text-xl mr-3"><IoIosArrowBack size={24} /></span>
+
+                <h2 className="text-white text-xl">Help & Support</h2>
+              </div>
+            </div>
+            <div className='pt-4 px-[10vw] mx-10 w-full text-[#4D4D4D] md:pt-0'>
+              <div className="w-full px-4 py-6 bg-white ">
+
+                <h2 className="text-[#4D4D4D] bg-white font-bold px--6">FAQs</h2>
+
+                <div className="flex flex-col items-center justify-center w-full space-y-4 bg-white">
+                  <div className="w-full relative p-3">
+                    <Accordion selectionMode="multiple">
+                      <AccordionItem key="1" aria-label="Kitna kama lete ho?" title="Kitna kama lete ho? ">
+                        {"Bas unicorn banne wale hai thode dino mein"}
+                      </AccordionItem>
+                      <AccordionItem key="2" aria-label="What are the delivery hours?" title="What are the delivery hours?">
+                        {"Our delivery hours are from 10:00 AM to 11:00 PM,seven days a week. Please note that delivery hours may vary slightly on public holidays"}
+                      </AccordionItem>
+                      <AccordionItem key="3" aria-label="What should I do if my payment is declined?" title="What should I do if my payment is declined?">
+                        {defaultContent}
+                      </AccordionItem>
+                      <AccordionItem key="4" aria-label="How do I request a refund?" title="How do I request a refund?">
+                        {defaultContent}
+                      </AccordionItem>
+                    </Accordion>
+                  </div>
+
+                </div>
+
+                <div className="flex flex-col items-center justify-center mt-20">
+                  <div className=""><p>For more queries email us at:</p></div>
+                  <div><p className="text-[#004BAD]">contact@fooddle.in</p></div>
+                </div>
+              </div>
+            </div>
+          </div>
         </div>
       </div>
+      <div className="hidden md:flex md:items-end md:w-[15vw]">
+        <p className="">
+          <img src="/i.png" alt="i" />
+          <span className="text-white">lorem</span>
+        </p>
       </div>
+
     </div>
   );
-};
+}
 
-export default Accordian;
+export default Accordian
